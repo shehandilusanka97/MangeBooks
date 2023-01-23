4c269fb28f6353895dabc135082b7e0e7512ada5
shehandilusanka97 <120352242+shehandilusankacristalcode@users.noreply.github.com>
2023-01-23 10:44:51 +0530

    commit

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -15,6 +15,15 @@ const Books = () => {
         }
         fetchAllBooks()
     },[])
+    const handleDelete= async(id)=>{
+        try{
+            await axios.delete("http://localhost:8800/books/"+id)
+            window.location.reload()
+
+        }catch(err){
+            console.log(err)
+        }
+    }
   return (
     <div>
         <h1>CK's BookShop</h1>
@@ -24,6 +33,8 @@ const Books = () => {
                {book.cover && <img src={book.cover} alt=""/>}
                <h2>{book.title}</h2>
                <p>{book.desc}</p>
+               <button className='delete' onClick={()=>handleDelete(book.id)}>Delete</button>
+               <button className='update'><Link to={`/update/${book.id}`}>Update</Link></button>
                 </div>
             ))}
         </div>
